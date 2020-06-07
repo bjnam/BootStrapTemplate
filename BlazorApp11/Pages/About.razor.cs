ed20bd688574f2a15d64da2d6e1d1b7a717232b6
bjnam <bjnam505@naver.com>
2020-06-07 13:07:19 +0900

    초기 커밋.

---
+++ BlazorApp11/Pages/About.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp11.Pages
+{
+    public partial class About
+    {
+        public string title { get; set; } = "회사소개";
+    }
+}
